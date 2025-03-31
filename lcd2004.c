41a8e7f4f675facd87804a7ed905db03c41edc44
Jav1ki4N <151114367+Jav1ki4N@users.noreply.github.com>
2025-04-01 03:07:09 +0800

    Add files via upload

--- lcd2004.c
+++ lcd2004.c
@@ -5,20 +5,15 @@
 /*     Source: ControllerTech   */
 
 #include "lcd2004.h"
+extern I2C_HandleTypeDef hi2c1; // Don't put this in the header file
 
-#define LCD_I2C_ADDR 0x4e // I2C device slave address
-                          // My adapter address is 0x4e
-
-extern I2C_HandleTypeDef hi2c1;
- 
-/*                   I2C Adapter Board                           */
+/*                 LCD Pins Config (may varies)                  */
 /* P7 - P4  : Data                                               */
 /* P3       : Useless, defalut 1                                 */
 /* P2, or EN: Enable LCD to receive data                         */
-/* P1, or RS: RS = 0, data is command; RS = 1, data is data      */
-/* P0, or RW: RW = 0, write; RW = 1, read                        */
+/* P1, or RW: RW = 0, write; RW = 1, read                        */
+/* P0, or RS: RS = 0, data is command; RS = 1, data is data      */
 /*                                                               */
-/* For write-only, the format should be xxxx 1100 or xxxx 1110   */
 /* A data or command is xxxx xxxx, you must send its high 4 Bits */
 /* and low 4 Bits seperately.                                    */
                                           
@@ -41,12 +36,12 @@ void LCD_Send_Command(char cmd) // e.g 0x3F
 
     /* Step.2 processing data */
     /* High 4 */
-    data_t[0] = data_h | 0x0C;    //EN = 1, RS = 0, RW = 0
-    data_t[1] = data_h | 0x08;    //EN = 0, RS = 0, RW = 0
+    data_t[0] = data_h | LCD_EN  | LCD_RS_COMMAND;    //EN = 1, RS = 0
+    data_t[1] = data_h | LCD_RS_COMMAND;              //EN = 0, RS = 0
 
     /* Low 4 */
-    data_t[2] = data_l | 0x0C;    //EN = 1, RS = 0, RW = 0
-    data_t[3] = data_l | 0x08;    //EN = 1, RS = 0, RW = 0
+    data_t[2] = data_l | LCD_EN  | LCD_RS_COMMAND;    //EN = 1, RS = 0
+    data_t[3] = data_l | LCD_RS_COMMAND;              //EN = 0, RS = 0
 
     /* step.3 Send data via I2C */
     HAL_I2C_Master_Transmit(&hi2c1,LCD_I2C_ADDR,data_t,4,100);
@@ -62,18 +57,18 @@ void LCD_Send_Data(char data)
     data_h = data        & 0xf0; 
     data_l = (data << 4) & 0xf0; 
                                                         
-    data_t[0] = data_h | 0x0D;    //EN = 1, RS = 0, RW = 0
-    data_t[1] = data_h | 0x09;    //EN = 0, RS = 0, RW = 0
+    data_t[0] = data_h | LCD_EN  | LCD_RS_DATA;    //EN = 1, RS = 0
+    data_t[1] = data_h | LCD_RS_DATA;              //EN = 0, RS = 0
 
-    data_t[2] = data_l | 0x0D;    //EN = 1, RS = 0, RW = 0
-    data_t[3] = data_l | 0x09;    //EN = 1, RS = 0, RW = 0
+    data_t[2] = data_l | LCD_EN  | LCD_RS_DATA;    //EN = 1, RS = 0
+    data_t[3] = data_l | LCD_RS_DATA;              //EN = 0, RS = 0
 
     HAL_I2C_Master_Transmit(&hi2c1,LCD_I2C_ADDR,data_t,4,100);
 }
 
 void LCD_Clear()
 {
-    LCD_Send_Command(0x01); 
+    LCD_Send_Command(LCD_CLEAR_FULL_SCREEN); 
 }
 
 /* Initialization Time Sequence */
@@ -96,15 +91,14 @@ void LCD_Init (void)
 	LCD_Send_Command (0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
 	HAL_Delay(1);
 	LCD_Send_Command (0x01);  // clear display
-	HAL_Delay(1);
-	HAL_Delay(1);
+	HAL_Delay(2);
 	LCD_Send_Command (0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
 	HAL_Delay(1);
 	LCD_Send_Command (0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
 }
 
 /* disaplay a string */
-void LCD_Disp_String(char *str)  //e.g. "Hell no, World!"
+void LCD_Disp_Str(char *str)  //e.g. "Hell no, World!"
 {
 	while (*str)                 //if the current char is not null
         LCD_Send_Data (*str++);  //keep on sending the next char
@@ -113,12 +107,28 @@ void LCD_Disp_String(char *str)  //e.g. "Hell no, World!"
 
 /************************************************************************************************/
 
-
 /* My customized functions */
 
-/* display a string on a specific line */
-void LCD_Disp_Line(int8_t line,char *str) // line is LINEx, where x could be 1-4
-{                                         // defined in the header file
-    LCD_Send_Command(line);
-    LCD_Disp_String(str);
+/* display a string on a specific Row */
+void LCD_Disp_String(int8_t row,int8_t col,char *str) 
+{
+    int8_t addr = row + (col-1);                                        
+    LCD_Send_Command(addr);
+    LCD_Disp_Str(str);
+}
+
+/* if you want to have a flickering row, you can use this function */
+void LCD_Blink_String(int8_t row,int8_t col,char *str,int period) //period is the period of flickering
+{
+  LCD_Disp_String(row,1,"                     ");
+  HAL_Delay(period);
+  LCD_Disp_String(row,col,str);
+  HAL_Delay(period);  
+}
+
+void LCD_Disp_Char(int8_t row,int8_t col,char ch)
+{
+    int8_t addr = row + (col-1);
+    LCD_Send_Command(addr);
+    LCD_Send_Data(ch);
 }
--- lcd2004.h
+++ lcd2004.h
@@ -1,16 +1,55 @@
 #ifndef _LCD2004_H_
 #define _LCD2004_H_
 
-/* LCD2004 Line address */
-#define LINE1 0x80|0x00
-#define LINE3 0x80|0x14
-#define LINE2 0x80|0x40
-#define LINE4 0x80|0x54
-
 #include "linker.h"
 
-void LCD_Init(void);
-void LCD_Clear(void);
-void LCD_Disp_Line(int8_t line,char *str);
+#define LCD_I2C_ADDR 0x4E // I2C device slave address
+                          // My adapter address is 0x4e
 
+#define ROW1 0x80|0x00    // LCD row address
+#define ROW3 0x80|0x14
+#define ROW2 0x80|0x40
+#define ROW4 0x80|0x54
+#define COL1 1
+#define COL2 2
+#define COL3 3
+#define COL4 4
+#define COL5 5
+#define COL6 6
+#define COL7 7
+#define COL8 8
+#define COL9 9
+#define COL10 10
+#define COL11 11
+#define COL12 12
+#define COL13 13
+#define COL14 14
+#define COL15 15
+#define COL16 16
+#define COL17 17
+#define COL18 18
+#define COL19 19
+#define COL20 20
+
+/* LCD Commands */                          
+#define LCD_CLEAR_FULL_SCREEN 0X01
+
+/* Data Mode */
+#define LCD_EN         0x0C //1100
+
+#define LCD_RW_WRITE   0X08 //1000
+#define LCD_RW_READ    0X0A //1010
+
+#define LCD_RS_COMMAND 0x08 //1000
+#define LCD_RS_DATA    0X09 //1001 
+
+void LCD_Init(void);
+
+void LCD_Send_Command(char cmd);
+void LCD_Send_Data(char data);
+
+void LCD_Clear(void);
+void LCD_Disp_String(int8_t row,int8_t col,char *str); 
+void LCD_Blink_String(int8_t row,int8_t col,char *str,int period);
+void LCD_Disp_Char(int8_t row,int8_t col,char ch);
 #endif
