95c0cc05eadc6184ad3389e8c1fa63fb058db790
Jav1ki4N <151114367+Jav1ki4N@users.noreply.github.com>
2025-04-01 15:36:34 +0800

    Update lcd2004.h

--- lcd2004.h
+++ lcd2004.h
@@ -41,15 +41,26 @@
 #define LCD_RW_READ    0X0A //1010
 
 #define LCD_RS_COMMAND 0x08 //1000
-#define LCD_RS_DATA    0X09 //1001 
+#define LCD_RS_DATA    0X09 //1001
 
+/* Customized Char */
+#define LCD_CGRAM_ADDR 0x40
+extern uint8_t heart_shape[];
 void LCD_Init(void);
 
 void LCD_Send_Command(char cmd);
 void LCD_Send_Data(char data);
 
 void LCD_Clear(void);
+
 void LCD_Disp_String(int8_t row,int8_t col,char *str); 
 void LCD_Blink_String(int8_t row,int8_t col,char *str,int period);
+
 void LCD_Disp_Char(int8_t row,int8_t col,char ch);
+void LCD_Blink_Char(int8_t row,int8_t col,char ch,int period);
+
+void LCD_Add_Custom_Char(uint8_t index, const uint8_t* char_buf);
+void LCD_Disp_Custom_Char(int8_t row,int8_t col,uint8_t index);
+void LCD_Blink_Custom_Char(int8_t row,int8_t col,uint8_t index,int period);
+
 #endif
